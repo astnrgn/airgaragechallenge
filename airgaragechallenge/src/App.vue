5f892266d3454120514389869dc44b6cb9edb8fb
astnrgn <austinmregan@gmail.com>
2021-03-25 19:07:16 -0400

    sort businesses with score low to high, display in template

--- airgaragechallenge/src/App.vue
+++ airgaragechallenge/src/App.vue
@@ -1,28 +1,95 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="search-header-and-input">
+      <p>Search Parking from Lowest to Highest</p>
+      <div class="search-input-and-button">
+        <input @input="getLocationInput" />
+        <button @click="getYelpLocation">Search</button>
+      </div>
+    </div>
+    <div>
+      <div
+        v-for="business in lowestToHighestParkingBusinesses"
+        :key="business.id"
+      >
+        <img
+          v-if="business.image_url"
+          :src="business.image_url"
+          :alt="business.name"
+        />
+        <p>
+          {{ business.location.display_address.join() }}
+        </p>
+        <p>
+          {{ business.review_count }}
+        </p>
+        <a :href="business.url" target="_blank">
+          {{ business.url }}
+        </a>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: {},
+  props: {},
+  data: function () {
+    return {
+      apiKey:
+        "mi5qSSqdhmrNXBjLq5MBMwuqcS0q8aE4u52fwqrG8CkrBjjksgdV8ZblHdh4ThtDqQVFapfOwrCqadcTH4sJIMhQgEcWpc0bK_9ms_rJ1H-xMT1Amp4tmH_PhAg3X3Yx",
+      location: "",
+      searchedParkingBusinesses: [],
+    };
+  },
+  computed: {
+    parkingBusinessesWithScore() {
+      return this.searchedParkingBusinesses.map((business) => {
+        let calculatedScore =
+          (business.review_count * business.rating) /
+          (business.review_count + 1);
+        return { ...business, score: calculatedScore };
+      });
+    },
+    lowestToHighestParkingBusinesses() {
+      return this.parkingBusinessesWithScore.concat().sort((a, b) => {
+        return a.score - b.score;
+      });
+    },
+  },
+  methods: {
+    getLocationInput(event) {
+      console.log("event", event.target.value);
+      this.location = event.target.value;
+    },
+    getYelpLocation() {
+      axios
+        .get(
+          "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search",
+          {
+            headers: {
+              Authorization: `Bearer ${this.apiKey}`,
+            },
+            params: {
+              location: this.location,
+              categories: "parking",
+            },
+          }
+        )
+        .then((res) => {
+          this.searchedParkingBusinesses = res.data.businesses;
+          console.log("res", res);
+        })
+        .catch((err) => {
+          console.log("err", err);
+        });
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
